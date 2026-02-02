5e0155f1c18e635d112ce4901905d35c5a83153d
Jang Seok Jin <162329005+JSJ426@users.noreply.github.com>
2026-02-02 14:20:40 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/ai-app
-nohup python3 -m uvicorn app.main:app --host 0.0.0.0 --port 8000 > /home/ec2-user/ai-app.log 2>&1 &
+nohup python3.11 -m uvicorn app.main:app --host 0.0.0.0 --port 8000 > /home/ec2-user/ai-app.log 2>&1 &
